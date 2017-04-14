f8fef7b0f46fa525c82d69a82f7dc46a487fc116
Alba Lamas <alba.v.lmas@gmail.com>
2017-04-14 22:21:43 +0200

    for smell equal to zero is finished

--- BarcenasWorld.pl
+++ BarcenasWorld.pl
@@ -2,12 +2,24 @@ intersectLocInfo( 0, _, 0 ).
 intersectLocInfo( _, 0, 0 ).
 intersectLocInfo( 1, Y, Y ).
 intersectLocInfo( X, 1, X ).
+
 intersectRow( [], [], [] ).
 intersectRow( [PH|PT], [NH|NT], [FH|FT] ) :-
              intersectLocInfo( PH, NH, FH ),
              intersectRow( PT, NT, FT ).
+
 intersectLocs( [], [], [] ).
 intersectLocs( [PrevRow|PrevLocs], [NewRow|NewLocs], FinalLocs ) :-
              intersectRow( PrevRow, NewRow, FinalRow ),
              intersectLocs( PrevLocs, NewLocs, RestOfRows ),
              FinalLocs = [ FinalRow | RestOfRows ].
+
+iSBarcenasAround( 1, 1, 0, [[0, 0, 1], [0, 1, 1], [1, 1, 1]] ).
+iSBarcenasAround( 1, 2, 0, [[0, 0, 0], [1, 0, 1], [1, 1, 1]] ).
+iSBarcenasAround( 1, 3, 0, [[0, 0, 0], [1, 1, 0], [1, 1, 1]] ).
+iSBarcenasAround( 2, 1, 0, [[0, 1, 1], [0, 0, 1], [0, 1, 1]] ).
+iSBarcenasAround( 2, 2, 0, [[0, 0, 1], [0, 0, 0], [1, 0, 1]] ).
+iSBarcenasAround( 2, 3, 0, [[0, 1, 0], [1, 0, 0], [1, 1, 0]] ).
+iSBarcenasAround( 3, 1, 0, [[0, 1, 1], [0, 1, 1], [0, 0, 1]] ).
+iSBarcenasAround( 3, 2, 0, [[0, 1, 1], [1, 0, 1], [0, 0, 0]] ).
+iSBarcenasAround( 3, 3, 0, [[0, 1, 1], [1, 1, 0], [1, 0, 0]] ).
