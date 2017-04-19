47889439a99a52b33aaee5f6585f555a498e996b
humbertValles <silmar22295@gmail.com>
2017-04-19 11:01:36 +0200

    Recursive function kinda working

--- BarcenasWorld2.pl
+++ BarcenasWorld2.pl
@@ -1,3 +1,5 @@
+:- use_module(library(lists)).
+
 intersectLocInfo( 0, _, 0 ).
 intersectLocInfo( _, 0, 0 ).
 intersectLocInfo( 1, Y, Y ).
@@ -19,25 +21,28 @@ isBarcenasAround( 2, 2, 0, [[0, 0, 1], [0, 0, 0], [1, 0, 1]] ).
 isBarcenasAround( 2, 3, 1, [[0, 0, 1], [0, 1, 1], [0, 0, 1]] ).
 
 isBarcenasOnLeft( 2, 3, -1, [[0, 0, 1], [0, 0, 0], [0, 0, 1]] ).
-isBarcenasOnLeft( 1, 2, 1, [[0, 0, 0], [1, 0, 0], [1, 0, 0]] ).
-isBarcenasOnLeft( 2, 2, 0, [[0, 1, 1], [0, 1, 1], [0, 1, 1]] ).
+isBarcenasOnLeft( 1, 2, 0, [[0, 0, 0], [1, 0, 0], [1, 0, 0]] ).
+isBarcenasOnLeft( 2, 2, 1, [[0, 1, 1], [0, 1, 1], [0, 1, 1]] ).
 
 updatePosBarcenasLocs( PrevLocs, AgentPosX, AgentPosY,  SmellXY, MarianoXY, FinalLocs )
    :-
       isBarcenasAround( AgentPosX, AgentPosY, SmellXY, AfterSmell ),
       intersectLocs( PrevLocs, AfterSmell, Locs ), !,
 	  isBarcenasOnLeft( AgentPosX, AgentPosY, MarianoXY, NewLocs ),
-	  intersectLocs( PrevLocs, NewLocs, FinalLocs ), !,
+	  intersectLocs( Locs, NewLocs, FinalLocs ), !,
       write( 'Estado resultante: ' ), write( FinalLocs ), nl.
 
 /*execSeqofSteps( [Step1,Step2, ... , StepN], FinalState ),*/
 /*execSeqofSteps( [[0,1,1],[1,1,1],[1,1,1]], [[1, 2, 0, 0, -1], [2, 2, 0, 1, 1], [2, 3, 1, -1, -1]], FS ).*/
-execSeqofSteps( _,[],FS ):-write( 'Estado resultante: ' ), write(FS FS ), nl.
+
+execSeqofSteps( FS, [], FS ):- write( 'Estado final: ' ), write(FS ), nl.
+
 execSeqofSteps( PrevLocs, [H|T], FS )
 	:-
 		nth0(0, H, X),
 		nth0(1, H, Y),
 		nth0(2, H, S),
 		nth0(3, H, M),
-		updatePosBarcenasLocs( PrevLocs, X, Y,  S, M, FinalLocs ),
-		execSeqogSteps(FinalLocs, T, FinalLocs )
+		write([X,Y,S,M]),
+		updatePosBarcenasLocs( PrevLocs, X, Y, S, M, NextLocs ),
+		execSeqofSteps( NextLocs, T, FS ).
